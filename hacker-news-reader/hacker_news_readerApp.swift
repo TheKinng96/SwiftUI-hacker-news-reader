2d19080a2258345ff2ef91b1b5d4d80ebbe2d48b
G3N <yap@corekara.co.jp>
2022-02-05 20:04:02 +0900

    Initial Commit

---
+++ hacker-news-reader/hacker_news_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacker_news_readerApp.swift
+//  hacker-news-reader
+//
+//  Created by Yap Feng Yuan on 2022/02/05.
+//
+
+import SwiftUI
+
+@main
+struct hacker_news_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
